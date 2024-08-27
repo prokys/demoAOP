5552c0a951e58f213f9421ea3d6fe2c00869a99c
Prokop Ferdan <prokes1@gmail.com>
2024-08-27 20:54:14 +0200

    added AOP dependency, updated application.properties to hide banner and reduce logging, added CommandLineRunner to main app

--- src/main/java/com/prokys/demoAOP/DemoAopApplication.java
+++ src/main/java/com/prokys/demoAOP/DemoAopApplication.java
@@ -1,13 +1,22 @@
 package com.prokys.demoAOP;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoAopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAopApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hello world");
+		};
 	}
 
 }
