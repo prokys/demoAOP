8678f59fd7327a872b285005e24af035d0aa0983
Prokop Ferdan <prokes1@gmail.com>
2024-08-28 17:07:55 +0200

    added demoTheAroundAdvice method to main app

--- src/main/java/com/prokys/demoAOP/DemoAopApplication.java
+++ src/main/java/com/prokys/demoAOP/DemoAopApplication.java
@@ -2,6 +2,7 @@ package com.prokys.demoAOP;
 
 import com.prokys.demoAOP.dao.AccountDAO;
 import com.prokys.demoAOP.dao.MembershipDAO;
+import com.prokys.demoAOP.service.TrafficFortuneService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,14 +18,27 @@ public class DemoAopApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(AccountDAO accountDAO, MembershipDAO membershipDAO){
+	public CommandLineRunner commandLineRunner(AccountDAO accountDAO, MembershipDAO membershipDAO, TrafficFortuneService trafficFortuneService){
 		return runner -> {
 
 //			demoTheBeforeAdvice(accountDAO, membershipDAO);
 //			demoTheAfterReturningAdvice(accountDAO);
 //			demoTheAfterThrowingAdvice(accountDAO);
-			demoTheAfterAdvice(accountDAO);
+//			demoTheAfterAdvice(accountDAO);
+			demoTheAroundAdvice(trafficFortuneService);
 		};
+	}
+
+	private void demoTheAroundAdvice(TrafficFortuneService trafficFortuneService) {
+		System.out.println("\nMain program: demoTheAroundAdvice");
+
+		System.out.println("Calling getFortune()");
+
+		String data = trafficFortuneService.getFortune();
+
+		System.out.println("\nMy fortune is: " + data);
+
+		System.out.println("Finished");
 	}
 
 	private void demoTheAfterAdvice(AccountDAO accountDAO) {
