79d84107adfe70952d38d090e42414b61f614b18
Prokop Ferdan <prokes1@gmail.com>
2024-08-28 10:48:30 +0200

    added @Pointcut declarations for methods starting with get and set, created combined @Pointcut declaration to exclude get and set methods, applied them to @Before advices

--- src/main/java/com/prokys/demoAOP/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/prokys/demoAOP/aspect/MyDemoLoggingAspect.java
@@ -15,15 +15,24 @@ public class MyDemoLoggingAspect {
     @Pointcut("execution(* com.prokys.demoAOP.dao.*.*(..))")
     private void forDAOPackage() {};
 
+    @Pointcut("execution(* com.prokys.demoAOP.dao.*.get*(..))")
+    private void forGetterMethods(){};
+
+    @Pointcut("execution(* com.prokys.demoAOP.dao.*.set*(..))")
+    private void forSetterMethods(){};
+
+    @Pointcut("forDAOPackage() && !(forGetterMethods() || forSetterMethods())")
+    private void forDaoPackageNoGetterSetter(){};
+
 
     // add @Before advice
-    @Before("forDAOPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(){
 
         System.out.println("\n =====>>>> Executing @Before advice on method");
 
     }
-    @Before("forDAOPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void performApiAnalytics(){
         System.out.println("\n =====>>>> Performing api analytics");
     }
